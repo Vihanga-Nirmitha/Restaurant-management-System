262b5aa3b6beb7c872a6842a9d2b4b4245d5d1a7
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-09-26 11:12:40 +0530

    Define controller

---
+++ README.md
@@ -0,0 +1,4 @@
+# RESTAURANT MANAGEMENT SYSTEM
+
+### Version
+0.1.0
--- restaurant-management-system/client-app/src/main/java/lk/ijse/dep11/controller/ClientViewController.java
+++ restaurant-management-system/client-app/src/main/java/lk/ijse/dep11/controller/ClientViewController.java
@@ -1,11 +1,14 @@
 package lk.ijse.dep11.controller;
 
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.dep11.shared.Item;
 import lk.ijse.dep11.shared.Order;
+
+import java.util.List;
 
 public class ClientViewController {
     public AnchorPane rootMain;
@@ -13,22 +16,68 @@ public class ClientViewController {
     public TextField txtOrderName;
     public TextField txtContactNo;
     public TextField txtItemName;
-    public TableView<Order> tblOrder;
-    public TableView tblStatus;
+    public TableView<Item> tblOrder;
+    public TableView<Order> tblStatus;
     public ImageView imvBurger;
     public ImageView imvPizza;
     public ImageView imvSub;
     public ImageView imvBurrito;
+    public Spinner<Integer> txtQty;
+    public Button btnAdd;
+    public Button btnDelete;
+    public Button btnPlaceOrder;
+    public void initialize(){
+        txtQty.setDisable(true);
+        txtQty.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1,100,1,1));
+        txtOrderID.setText(generateOrderId());
 
-    public void imvBurgerOnMouseCliked(MouseEvent mouseEvent) {
+    }
+
+    private String generateOrderId() {
+        if(getAllOrders().isEmpty()) return "ODR-0001";
+        else {
+            int nextOrder = Integer.parseInt(tblStatus.getItems().get(getAllOrders().size()-1).getOderId().substring(4)) +1; 
+            return String.format("ODR-%04d",nextOrder);
+        }
+    }
+
+    public void imvBurgerOnMouseClicked(MouseEvent mouseEvent) {
+        txtQty.setDisable(false);
+        txtItemName.setText("Burger");
+        System.out.println("Clicked");
+        mouseEvent.consume();
     }
 
     public void imvPizzaOnMouseClicked(MouseEvent mouseEvent) {
+        txtQty.setDisable(false);
+        txtItemName.setText("Pizza");
+        System.out.println("Clicked");
+        mouseEvent.consume();
     }
 
     public void imvSubOnMouseClicked(MouseEvent mouseEvent) {
+        txtQty.setDisable(false);
+        txtItemName.setText("Submerine");
+        System.out.println("Clicked");
+        mouseEvent.consume();
     }
 
     public void imvBurritoOnMouseClicked(MouseEvent mouseEvent) {
+        txtQty.setDisable(false);
+        txtItemName.setText("Burrito");
+        System.out.println("Clicked");
+        mouseEvent.consume();
+    }
+
+    public void btnAddOnAction(ActionEvent event) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent event) {
+    }
+
+    public void btnPlaceOrderOnAction(ActionEvent event) {
+    }
+    public List<Order> getAllOrders(){
+        return tblStatus.getItems();
     }
 }
